bbb640f890618dc0295c301affd73ebd68e68e26
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-06 22:46:14 -0700

    set up jwt

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // Import controllers
-const { signup } = require('../controllers/user');
+const { signup, signin } = require('../controllers/user');
 const { userSignupValidator } = require('../validator/index');
 
 router.post('/signup', userSignupValidator, signup);
+router.post('/signin', signin);
 
 module.exports = router;
