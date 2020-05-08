41ba5a49ee9dcaec7d6373fe75c1f73b013691bd
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-08 16:19:49 -0700

    create product validations

--- controllers/product.js
+++ controllers/product.js
@@ -1,32 +1,64 @@
-const Product = require('../models/product');
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
+const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
-    from.keepExtensions = true;
+    form.keepExtensions = true;
     form.parse(req, (err, fields, files) => {
         if (err) {
             return res.status(400).json({
-                error: 'Upload failed'
+                error: 'Image could not be uploaded'
             });
         }
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
         let product = new Product(fields);
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1MB in size'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
+
         product.save((err, result) => {
             if (err) {
+                console.log('Could not create product ', err);
                 return res.status(400).json({
                     error: errorHandler(err)
                 });
             }
-            res.json({
-                result
-            });
+            res.json(result);
         });
     });
 };
