88fcc71cd87ed60aff51f4d8208b0de7192a3d24
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-09 00:35:30 -0700

    created product delete controller func and route

--- controllers/product.js
+++ controllers/product.js
@@ -84,3 +84,18 @@ exports.create = (req, res) => {
         });
     });
 };
+
+// Remove a product by Id and user role condition: admin
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Product deleted successfully'
+        });
+    });
+};
--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 // Import controllers
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { create, productById, read } = require('../controllers/product');
+const { create, productById, read, remove } = require('../controllers/product');
 const { userById } = require('../controllers/user');
 
 // Category routes
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete(
+    '/product/:productId/:userId',
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
 
 // Router params
 router.param('userId', userById);
