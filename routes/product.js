ef21411ab9c2571e6c48bb250c7ec618ae798d05
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-09 00:20:13 -0700

    created product by id

--- routes/product.js
+++ routes/product.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // Import controllers
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { create } = require('../controllers/product');
+const { create, productById, read } = require('../controllers/product');
 const { userById } = require('../controllers/user');
 
 // Category routes
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+// Router params
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
